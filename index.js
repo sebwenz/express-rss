8955e649994d44ef94f6b310858c9939f59ff48d
Sebastian Wenz <sebastian@Sebastians-MacBook-Pro.local>
2019-08-11 12:46:50 +0200

    funktionierender shit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let qs = require('querystring');
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"))
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
     rss.load('http://www.maclife.de/rss/news.xml', function (err, rss) {
@@ -14,28 +15,13 @@ app.get('/', (req, res) => {
     });
 });
 
-
-function loadFeed (request, response){
-    if (request.method == 'POST') {
-        var body = '';
-
-        request.on('data', function (data) {
-            body += data;
-
-            // Too much POST data, kill the connection!
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6)
-                request.connection.destroy();
-        });
-
-        request.on('end', function () {
-            var post = qs.parse(body);
-            rss.load(toString(post.rss-feed-link));
-            response.render('index', {rss: rss.items})
-            console.log(rss);
-        });
-    }
-}
+app.post('/addFeed', (req, res) => {
+    const feedLink = req.body.rssfeedlink;
+    rss.load(feedLink, function (err, rss){
+        res.render('index', {rss: rss.items});
+        console.log(rss);
+    });
+});
 
 app.listen(8080, () => {
     console.log('app listening on port 8080');
