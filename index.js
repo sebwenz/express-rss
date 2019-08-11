47d0d59e9f8c7ce02c5fbe923ed3a681f5b4693b
Sebastian Wenz <sebastian@Sebastians-MacBook-Pro.local>
2019-08-11 10:51:17 +0200

    initial troll commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+let rss = require('rss-to-json');
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(express.static("node_modules/bootstrap/dist"))
+
+app.get('/', (req, res) => {
+    rss.load('http://www.maclife.de/rss/news.xml', function (err, rss) {
+        res.render('index', { rss: rss.items });
+        console.log(rss);
+    });
+
+});
+
+app.listen(8080, () => {
+    console.log('app listening on port 8080');
+});
+
+
+
+//https://rss.golem.de/rss.php?feed=RSS2.0
